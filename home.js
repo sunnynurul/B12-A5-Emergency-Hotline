0ed413837679e08a6ee28e0103193d8a9e59c8e7
Nurul Sunny <sunnynurul03@gmail.com>
2025-08-29 10:08:00 -0400

    design complete

---
+++ home.js
@@ -0,0 +1,66 @@
+document.getElementById("heart-1").addEventListener("click",function(e)
+{
+    e.preventDefault();
+    console.log("heart clicked-1.");
+})
+
+document.getElementById("heart-2").addEventListener("click",function(e)
+{
+    e.preventDefault();
+    console.log("heart clicked-2.");
+})
+
+
+document.getElementById("heart-3").addEventListener("click",function(e)
+{
+    e.preventDefault();
+    console.log("heart clicked-3.");
+})
+
+
+document.getElementById("heart-4").addEventListener("click",function(e)
+{
+    e.preventDefault();
+    console.log("heart clicked-4.");
+})
+
+
+document.getElementById("heart-5").addEventListener("click",function(e)
+{
+    e.preventDefault();
+    console.log("heart clicked-5.");
+})
+
+
+document.getElementById("heart-6").addEventListener("click",function(e)
+{
+    e.preventDefault();
+    console.log("heart clicked-6.");
+})
+
+
+document.getElementById("heart-7").addEventListener("click",function(e)
+{
+    e.preventDefault();
+    console.log("heart clicked-7.");
+})
+
+
+document.getElementById("heart-8").addEventListener("click",function(e)
+{
+    e.preventDefault();
+    console.log("heart clicked-8.");
+})
+
+document.getElementById("heart-9").addEventListener("click",function(e)
+{
+    e.preventDefault();
+    console.log("heart clicked-9.");
+})
+
+
+
+
+
+
+

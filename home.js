6e914c586c07629d0ac08efa9d725e9ef062ea8a
Nurul Sunny <sunnynurul03@gmail.com>
2025-09-01 11:27:00 -0400

    Complete heart count funtionality

--- home.js
+++ home.js
@@ -1,66 +1,94 @@
-document.getElementById("heart-1").addEventListener("click",function(e)
+ 
+ 
+ 
+ 
+document.getElementById("heart-1").addEventListener("click",function(e)     
 {
-    e.preventDefault();
-    console.log("heart clicked-1.");
+  e.preventDefault()
+  let count=0;
+  count++;
+  const heartNumCount=parseInt(document.getElementById("heart-num-count").innerText)
+  const heartNumCountTotal=count+heartNumCount;
+  document.getElementById("heart-num-count").innerText=heartNumCountTotal
 })
 
-document.getElementById("heart-2").addEventListener("click",function(e)
+document.getElementById("heart-2").addEventListener("click",function(e)     
 {
-    e.preventDefault();
-    console.log("heart clicked-2.");
+  e.preventDefault()
+  let count=0;
+  count++;
+  const heartNumCount=parseInt(document.getElementById("heart-num-count").innerText)
+  const heartNumCountTotal=count+heartNumCount;
+  document.getElementById("heart-num-count").innerText=heartNumCountTotal
 })
 
-
-document.getElementById("heart-3").addEventListener("click",function(e)
+document.getElementById("heart-3").addEventListener("click",function(e)     
 {
-    e.preventDefault();
-    console.log("heart clicked-3.");
+  e.preventDefault()
+  let count=0;
+  count++;
+  const heartNumCount=parseInt(document.getElementById("heart-num-count").innerText)
+  const heartNumCountTotal=count+heartNumCount;
+  document.getElementById("heart-num-count").innerText=heartNumCountTotal
 })
 
-
-document.getElementById("heart-4").addEventListener("click",function(e)
+document.getElementById("heart-4").addEventListener("click",function(e)     
 {
-    e.preventDefault();
-    console.log("heart clicked-4.");
+  e.preventDefault()
+  let count=0;
+  count++;
+  const heartNumCount=parseInt(document.getElementById("heart-num-count").innerText)
+  const heartNumCountTotal=count+heartNumCount;
+  document.getElementById("heart-num-count").innerText=heartNumCountTotal
 })
 
-
-document.getElementById("heart-5").addEventListener("click",function(e)
+document.getElementById("heart-5").addEventListener("click",function(e)     
 {
-    e.preventDefault();
-    console.log("heart clicked-5.");
+  e.preventDefault()
+  let count=0;
+  count++;
+  const heartNumCount=parseInt(document.getElementById("heart-num-count").innerText)
+  const heartNumCountTotal=count+heartNumCount;
+  document.getElementById("heart-num-count").innerText=heartNumCountTotal
 })
 
-
-document.getElementById("heart-6").addEventListener("click",function(e)
+document.getElementById("heart-6").addEventListener("click",function(e)     
 {
-    e.preventDefault();
-    console.log("heart clicked-6.");
+  e.preventDefault()
+  let count=0;
+  count++;
+  const heartNumCount=parseInt(document.getElementById("heart-num-count").innerText)
+  const heartNumCountTotal=count+heartNumCount;
+  document.getElementById("heart-num-count").innerText=heartNumCountTotal
 })
 
-
-document.getElementById("heart-7").addEventListener("click",function(e)
+document.getElementById("heart-7").addEventListener("click",function(e)     
 {
-    e.preventDefault();
-    console.log("heart clicked-7.");
+  e.preventDefault()
+  let count=0;
+  count++;
+  const heartNumCount=parseInt(document.getElementById("heart-num-count").innerText)
+  const heartNumCountTotal=count+heartNumCount;
+  document.getElementById("heart-num-count").innerText=heartNumCountTotal
 })
 
-
-document.getElementById("heart-8").addEventListener("click",function(e)
+document.getElementById("heart-8").addEventListener("click",function(e)     
 {
-    e.preventDefault();
-    console.log("heart clicked-8.");
+  e.preventDefault()
+  let count=0;
+  count++;
+  const heartNumCount=parseInt(document.getElementById("heart-num-count").innerText)
+  const heartNumCountTotal=count+heartNumCount;
+  document.getElementById("heart-num-count").innerText=heartNumCountTotal
 })
 
-document.getElementById("heart-9").addEventListener("click",function(e)
+document.getElementById("heart-9").addEventListener("click",function(e)     
 {
-    e.preventDefault();
-    console.log("heart clicked-9.");
+  e.preventDefault()
+  let count=0;
+  count++;
+  const heartNumCount=parseInt(document.getElementById("heart-num-count").innerText)
+  const heartNumCountTotal=count+heartNumCount;
+  document.getElementById("heart-num-count").innerText=heartNumCountTotal
 })
-
-
-
-
-
-
 
